10b3057b8ad5e81cb659bfe046c68bdc5e3ed045
koffydrop <67888720+koffydrop@users.noreply.github.com>
2024-11-30 20:00:34 -0300

    add vencord window injections

--- src/common/desktopCoreTemplates.js
+++ src/common/desktopCoreTemplates.js
@@ -1,4 +1,10 @@
 const preloadScaffold = `
+const fs = require("original-fs"); // using electron's fs causes app.asar to be locked during host updates
+
+const VENCORD_DIR = path.join(electron.app.getPath("userData"), "..", "Vencord");
+const vencord_settings_file = fs.readFileSync(path.join(VENCORD_DIR, "settings", "settings.json"), "utf8");
+const vencord_settings = JSON.parse(vencord_settings_file);
+
 electron.ipcMain.on("SHELTUPDATE_FRAMEWORK_ORIGINAL_PRELOAD", (event) => {
 	event.returnValue = event.sender.originalPreload;
 });
@@ -13,10 +19,37 @@ const ProxiedBrowserWindow = new Proxy(electron.BrowserWindow, {
       // We replace the preload instead of using setPreloads because of some
       // differences in internal behaviour.
       options.webPreferences.preload = path.join(__dirname, "preload.js");
-    }
 
+      // vencord stuff
+      if (vencord_settings.frameless) {
+        options.frame = false;
+      } else if (process.platform === "win32" && vencord_settings.winNativeTitleBar) {
+        delete options.frame;
+      }
+
+      if (vencord_settings.transparent) {
+        options.transparent = true;
+        options.backgroundColor = "#00000000";
+      }
+
+      const needsVibrancy = process.platform === "darwin" && vencord_settings.macosVibrancyStyle;
+
+      if (needsVibrancy) {
+        options.backgroundColor = "#00000000";
+        if (vencord_settings.macosVibrancyStyle) {
+          options.vibrancy = vencord_settings.macosVibrancyStyle;
+        }
+      }
+    }
     const window = new target(options);
     window.webContents.originalPreload = originalPreload;
+    window.webContents.on("devtools-opened", () => {
+      if (!electron.nativeTheme.shouldUseDarkColors) return;
+      electron.nativeTheme.themeSource = "light";
+      setTimeout(() => {
+        electron.nativeTheme.themeSource = "dark";
+      }, 100);
+    });
     return window;
   },
 });
@@ -30,7 +63,7 @@ require.cache[electronPath].exports = {
 `;
 
 export const finalizeDesktopCoreIndex = (patches, hasPreload) =>
-	`// SHELTUPDATE PATCH FRAMEWORK INDEX HEADER:
+  `// SHELTUPDATE PATCH FRAMEWORK INDEX HEADER:
 {
 const electron = require("electron");
 const path = require("path");
@@ -41,10 +74,10 @@ ${hasPreload ? preloadScaffold : ""}
 ${patches}
 
 // END PATCHES, CHAINLOAD:
-module.exports = require('./core.asar');`
+module.exports = require('./core.asar');`;
 
 export const finalizeDesktopCorePreload = (preloads) =>
-	`// SHELTUPDATE PATCH FRAMEWORK PRELOAD HEADER:
+  `// SHELTUPDATE PATCH FRAMEWORK PRELOAD HEADER:
 {
 const { ipcRenderer } = require("electron");
 
